cafecdde8fbed6238d42c83eebf1f13e180330a7
mayska <alexandre.meunier31@gmail.com>
2022-08-06 14:39:12 +0200

    New (zod): Add zod for check schema in dto.

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,7 +1,14 @@
+import { z } from "zod"
+
+const userSchema = z.object({
+    id:z.optional(z.string()),
+    name:z.string()
+})
 export class User {
     id: string
     name: string
     constructor(data: any) {
+        userSchema.parse(data)
         this.id = data.id
         this.name = data.name
     }
